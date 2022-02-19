a73af690486b22190d952e5764d5c6c99a82c94a
Axeler4t0r <me@k2t3k.tk>
2022-02-19 10:55:15 +0000

    Feat: add CORSMiddleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,18 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.http.server.configuration.port = 2080
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    let error = ErrorMiddleware.default(environment: app.environment)
+    // 清除现有的 middleware。
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(error)
+
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateAppInfo())
