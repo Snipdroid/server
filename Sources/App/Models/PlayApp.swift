12a1711f64b7f55bca668e5ed7aa671d5081c9de
Butanediol <me@k2t3k.tk>
2022-02-24 13:48:44 +0000

    Fix: placeholder image url

--- Sources/App/Models/PlayApp.swift
+++ Sources/App/Models/PlayApp.swift
@@ -6,5 +6,5 @@ struct PlayApp: Content {
     var url: String
     var image: String
 
-    static let placeholder = PlayApp(name: "Placeholder", url: "https://app-tracker.k2t3k.tk", image: "https://cloudreve.butanediol.me/api/v3/file/source/18/NB_Icon_Mask_Shapes_Ext_02.gif?sign=aZPSh9NjBJl_jpFPNKtbefksgEOJ_IHb2FpBiWpjBoA%3D%3A0")
+    static let placeholder = PlayApp(name: "Placeholder", url: "https://app-tracker.k2t3k.tk", image: "https://app-tracker.k2t3k.tk/static/placeholder.gif")
 }
