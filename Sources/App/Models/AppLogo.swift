d89cb4dc8efc653c98784511b573effbaabfef71
Butanediol <me@k2t3k.tk>
2022-03-04 14:17:27 +0000

    Fix: change placeholder  logo to svg

--- Sources/App/Models/AppLogo.swift
+++ Sources/App/Models/AppLogo.swift
@@ -6,7 +6,7 @@ struct AppLogo: Content {
     var url: String
     var image: String
 
-    static let placeholder = AppLogo(name: "Placeholder", url: "https://app-tracker.k2t3k.tk", image: "https://app-tracker.k2t3k.tk/static/placeholder.gif")
+    static let placeholder = AppLogo(name: "Placeholder", url: "https://app-tracker.k2t3k.tk", image: "https://source.android.com/setup/images/Android_symbol_green_RGB.svg")
 
     func wrapped() -> AppLogo {
         let newImageUrl = "https://download-proxy.butanediol.workers.dev/?url=\(self.image.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self.image )"
