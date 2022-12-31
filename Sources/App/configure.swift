6fb1bdb51cda931f75a6fc96379c0951684b0481
Butanediol <iam@butanediol.me>
2022-12-31 12:39:10 +0800

    Feat: log info for storage system

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -13,16 +13,20 @@ public func configure(_ app: Application) throws {
 
     // S3
     if let bucket = Environment.get("S3_BUCKET"),
-        let accessKeyId = Environment.get("S3_ACCESS_KEY_ID"),
-        let secretAccessKey = Environment.get("S3_SECRET_ACCESS_KEY") {
-            app.iconProvider = S3IconProvider.init(
-                bucket: bucket, 
-                accessKeyId: accessKeyId, 
-                secretAccessKey: secretAccessKey,
-                s3Region: .init(rawValue: Environment.get("S3_REGION")),
-                s3Endpoint: Environment.get("S3_ENDPOINT")
-            )
-            app.lifecycle.use(S3LifecycleHandler())
+       let accessKeyId = Environment.get("S3_ACCESS_KEY_ID"),
+       let secretAccessKey = Environment.get("S3_SECRET_ACCESS_KEY")
+    {
+        app.iconProvider = S3IconProvider(
+            bucket: bucket,
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+            s3Region: .init(rawValue: Environment.get("S3_REGION")),
+            s3Endpoint: Environment.get("S3_ENDPOINT")
+        )
+        app.lifecycle.use(S3LifecycleHandler())
+        app.logger.info("Using S3 storage.")
+    } else {
+        app.logger.info("Using local storage. Please make sure ./data/icons directory exists.")
     }
 
     guard let postgresUrl = Environment.get("POSTGRES_URL") else {
@@ -32,22 +36,23 @@ public func configure(_ app: Application) throws {
     try app.databases.use(.postgres(url: postgresUrl), as: .psql)
     app.migrations.add(CreateAppInfo())
 
-    if let httpProxyAddr = Environment.get("HTTP_PROXY_ADDR"), 
-        let httpProxyPort = Environment.get("HTTP_PROXY_PORT"),
-        let httpProxyPortNumber = Int(httpProxyPort) {
-            app.logger.info("Using http proxy http://\(httpProxyAddr):\(httpProxyPortNumber)")
-            app.http.client.configuration.proxy = .server(host: httpProxyAddr, port: httpProxyPortNumber)
+    if let httpProxyAddr = Environment.get("HTTP_PROXY_ADDR"),
+       let httpProxyPort = Environment.get("HTTP_PROXY_PORT"),
+       let httpProxyPortNumber = Int(httpProxyPort)
+    {
+        app.logger.info("Using http proxy http://\(httpProxyAddr):\(httpProxyPortNumber)")
+        app.http.client.configuration.proxy = .server(host: httpProxyAddr, port: httpProxyPortNumber)
     }
 
     // register routes
     try routes(app)
 }
 
-fileprivate func configureHttp(_ app: Application) {
+private func configureHttp(_ app: Application) {
     app.http.server.configuration.port = 2080
 }
 
-fileprivate func configureMiddleware(_ app: Application) {
+private func configureMiddleware(_ app: Application) {
     let corsConfiguration = CORSMiddleware.Configuration(
         allowedOrigin: .all,
         allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
