a8a0502d7b02e0de02634396f2c42031312bc889
Butanediol <iam@butanediol.me>
2023-01-21 22:39:11 +0800

    Fix: mismatched model during migration

--- Sources/App/Migrations/2-RemoveSignature.swift
+++ Sources/App/Migrations/2-RemoveSignature.swift
@@ -1,10 +1,36 @@
 import Fluent
 import Vapor
 
+private class AppInfoLegacy: Model, Equatable {
+    static func == (lhs: AppInfoLegacy, rhs: AppInfoLegacy) -> Bool {
+        lhs.packageName == rhs.packageName &&
+        lhs.activityName == rhs.activityName
+    }
+    
+    static let schema = "app_infos"
+    
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "app_name")
+    var appName: String
+
+    @Field(key: "package_name")
+    var packageName: String
+
+    @Field(key: "activity_name")
+    var activityName: String
+    
+    @Field(key: "count")
+    var count: Int
+    
+    required init() { }
+}
+
 struct RemoveSignature: AsyncMigration {
     func prepare(on database: Database) async throws {
 
-        let appInfoList = try await AppInfo.query(on: database).all()
+        let appInfoList = try await AppInfoLegacy.query(on: database).all()
         for index in appInfoList.indices {
             if appInfoList[index...].filter({ $0 == appInfoList[index] }).count > 1 {
                 try await appInfoList[index].delete(on: database)
